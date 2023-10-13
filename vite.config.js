4a7e50aa499883169b6546ba743925caa7eebc33
Sunny <kumarsunny63588@gmail.com>
2023-10-13 13:45:36 +0530

    edit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Celebrity-Booking/",
+  // base:"/Celebrity-Booking/",
   plugins: [react()],
 })
